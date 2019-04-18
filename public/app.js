95f5b0f216cf4376daad917a0314db9590b5849e
sespertg <sespert@gmail.com>
2019-04-17 22:39:39 -0400

    Fixed layout

--- public/app.js
+++ public/app.js
@@ -4,8 +4,8 @@ $.getJSON("/all", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<div class='card'><div class='card-body'><h5 class='card-title'>" + data[i].title
-      + "</h5><a href='" + data[i].link + "' class='card-link' target='_blank'>Link to the Article</a><button id='noteBtn' data-id='" + data[i]._id 
+      $("#articles").append("<div class='card bg-warning mb-3 text-center'><div class='card-body'><h5 class='card-title'>" + data[i].title
+      + "</h5><a href='" + data[i].link + "' class='card-link' target='_blank'>Link to the Article</a><br><button id='noteBtn' data-id='" + data[i]._id 
       + "'>Add Note</button></div></div>");    }
   });
   
@@ -21,7 +21,7 @@ $("#clear").on("click", function(){
   // Whenever someone clicks a p tag
   $(document).on("click", "#noteBtn", function() {
     // Empty the notes from the note section
-    $("#notes").empty();
+    $("#articles").hide();
     // Save the id from the p tag
     var thisId = $(this).attr("data-id");
   
@@ -33,15 +33,18 @@ $("#clear").on("click", function(){
       // With that done, add the note information to the page
       .then(function(data) {
         console.log(data);
+
+
+        var divCard = $("<div class='card bg-warning mb-3 text-center center'><div class='card-body'>");
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
+        divCard.append("<h2 class='card-title'>" + data.title + "</h2>");
         // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
+        divCard.append("<input id='titleinput' name='title' >");
         // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        divCard.append("<textarea id='bodyinput' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
+        divCard.append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        $("#notes").append(divCard);
         // If there's a note in the article
         if (data.note) {
           // Place the title of the note in the title input
@@ -54,6 +57,8 @@ $("#clear").on("click", function(){
   
   // When you click the savenote button
   $(document).on("click", "#savenote", function() {
+   
+
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
   
@@ -74,6 +79,7 @@ $("#clear").on("click", function(){
         console.log(data);
         // Empty the notes section
         $("#notes").empty();
+        $("#articles").show();
       });
   
     // Also, remove the values entered in the input and textarea for note entry
@@ -87,9 +93,9 @@ $("#clear").on("click", function(){
         // For each one
         for (var i = 0; i < data.length; i++) {
           // Display the apropos information on the page
-          $("#articles").append("<div class='card'><div class='card-body'><h5 class='card-title'>" + data[i].title
-          + "</h5><a href='" + data[i].link + "' class='card-link' target='_blank'>Link to the Article</a><button id='noteBtn' data-id='" + data[i]._id 
-          + "'>Add Note</button></div></div>");    }
+          $("#articles").append("<div class='card bg-warning mb-3 text-center'><div class='card-body'><h5 class='card-title'>" + data[i].title
+      + "</h5><a href='" + data[i].link + "' class='card-link' target='_blank'>Link to the Article</a><br><button id='noteBtn' data-id='" + data[i]._id 
+      + "'>Add Note</button></div></div>");    }
       });
   }
   
